3aa79935531fff13fb917c6a753287fad3922b20
TValeriiaM <149121749+TValeriiaM@users.noreply.github.com>
2024-06-19 23:28:39 +0300

    shorting_code

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,6 +2,15 @@ import { createSlice, createSelector } from "@reduxjs/toolkit";
 import { addContact, fetchContacts, deleteContact } from "./contactsOps";
 import { selectNameFilter } from "./filtersSlice";
 
+const handlePending = (state) => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
 const contactsSlice = createSlice({
   name: "contacts",
   initialState: {
@@ -11,44 +20,26 @@ const contactsSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-      .addCase(fetchContacts.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
+      .addCase(fetchContacts.pending, handlePending)
       .addCase(fetchContacts.fulfilled, (state, action) => {
         state.items = action.payload;
         state.loading = false;
       })
-      .addCase(fetchContacts.rejected, (state) => {
-        state.loading = false;
-        state.error = true;
-      })
-      .addCase(addContact.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
+      .addCase(fetchContacts.rejected, handleRejected)
+      .addCase(addContact.pending, handlePending)
       .addCase(addContact.fulfilled, (state, action) => {
         state.items.push(action.payload);
         state.loading = false;
       })
-      .addCase(addContact.rejected, (state) => {
-        state.loading = false;
-        state.error = true;
-      })
-      .addCase(deleteContact.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
+      .addCase(addContact.rejected, handleRejected)
+      .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
         state.items = state.items.filter(
           (item) => item.id !== action.payload.id
         );
         state.loading = false;
       })
-      .addCase(deleteContact.rejected, (state) => {
-        state.loading = false;
-        state.error = true;
-      });
+      .addCase(deleteContact.rejected, handleRejected);
   },
 });
 
